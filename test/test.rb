d17f6c22b611245005d6cdca58fde7e4271bfb59
m1a2023 <mag2017go@gmail.com>
2024-08-14 22:42:11 +0500

    added Month, Week, Day -classes

---
+++ test/test.rb
@@ -0,0 +1,11 @@
+require_relative '../src/parser.rb'
+require "json"
+
+d = Day.initialize
+puts "DAY\n", d, "\n"
+
+w = Week.initialize
+puts "WEEK\n", w, "\n"
+
+m = Month.initialize
+puts "MONTH\n", m, "\n"
